01b7ebf8e87a2dc0ee255aefea84c6a1fcf80338
trippel <tr@ewu-software.com>
2016-08-22 06:41:47 +0200

    removed .dep

--- .dep/obj_gh60_lufa_common_avr_eeconfig.o.d
+++
@@ -1,6 +0,0 @@
-obj_gh60_lufa/common/avr/eeconfig.o: ../../tmk_core/common/avr/eeconfig.c \
- config.h ../../tmk_core/common/eeconfig.h
-
-config.h:
-
-../../tmk_core/common/eeconfig.h:
--- .dep/obj_gh60_lufa_led.o.d
+++
@@ -1,5 +0,0 @@
-obj_gh60_lufa/led.o: led.c config.h ../../tmk_core/common/led.h
-
-config.h:
-
-../../tmk_core/common/led.h:
